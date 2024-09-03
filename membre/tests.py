169c31de7f00e7fc0ef8b3b4d85d167f1ef8a73e
Bara87 <bara-2010@hotmail.fr>
2024-09-03 13:43:57 +0200

    edit tests

--- membre/tests.py
+++ membre/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+from django.urls import reverse
+from bibliothecaire.models import Media
 
-# Create your tests here.
+class MembreViewsTestCase(TestCase):
+    
+    def setUp(self):
+        """Prépare les objets nécessaires pour les tests."""
+        # Crée un média pour les tests
+        self.media = Media.objects.create(nom="Le Grand Livre", disponible=True)
+    
+    def test_liste_medias_view(self):
+        """Teste la vue de la liste des médias."""
+        # Effectue une requête GET sur la vue 'liste_medias'
+        response = self.client.get(reverse('liste_medias'))
+        
+        # Vérifie que le code de réponse HTTP est 200 OK
+        self.assertEqual(response.status_code, 200)
+        
+        # Vérifie que le bon template est utilisé
+        self.assertTemplateUsed(response, 'membre/liste_medias.html')
+        
+        # Vérifie que le média créé est affiché dans la réponse
+        self.assertContains(response, self.media.nom)
